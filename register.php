f907e09c1edaed1b89cc1c3fdc120ec994c1c88c
rober0 <115416432+rober0@users.noreply.github.com>
2025-04-09 19:15:37 -0300

    Add files via upload
    Traduções, correção do registro.html e mais

--- register.php
+++ register.php
@@ -11,33 +11,7 @@
 
         $query = "INSERT INTO userregistro (username, email, password, ocupacao) 
             VALUES ('$Nome', '$Email', '$Senha', '$Ocupacao')";
-        // Verifica se o usuário já existe
-        $verifica = mysqli_query($conexao, "SELECT * FROM userregistro WHERE email = '$Email'");
-        if (mysqli_num_rows($verifica) > 0) {
-            echo "Usuário já cadastrado!";
-            exit;
-        }
-        // Verifica se a senha tem mais do que 45 caracteres
-        if (strlen($Senha) > 45) {
-            echo "A senha não pode ter mais do que 45 caracteres!";
-            exit;
-        }
-        // Verifica se o nome de usuário tem pelo menos 45 caracteres
-        if (strlen($Nome) < 3) {
-            echo "O nome de usuário deve ter pelo menos 45 caracteres!";
-            exit;
-        }
-        // Verifica se o email é válido
-        if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
-            echo "Email inválido!";
-            exit;
-        }
-        // Verifica se a ocupação é válida
-        $ocupacoes_validas = ['paciente', 'medico'];
-        if (!in_array($Ocupacao, $ocupacoes_validas)) {
-            echo "Ocupação inválida!";
-            exit;
-        }
+
         mysqli_query($conexao, $query);
 
         header("Location: register.html");
